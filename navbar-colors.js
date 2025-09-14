5533cd1ffb856ccbee4b2cc6ef4e5aa4dbb29630
LIM JANGBIN <great_jb2005@naver.com>
2025-09-14 13:12:57 +0900

    Create navbar-colors.js

---
+++ navbar-colors.js
@@ -0,0 +1,11 @@
+<script>
+  const logo = document.querySelector('.logo-first');
+
+  if (location.pathname.includes('press')) {
+    logo.style.color = 'blue';
+  } else if (location.pathname.includes('exhibitions')) {
+    logo.style.color = '#EC9F53';
+  } else if (location.pathname.includes('index')) {
+    logo.style.color = 'red';
+  }
+</script>
